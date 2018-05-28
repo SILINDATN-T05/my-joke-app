6a1ab3f31466b2914ed1a621a48251f51175adde
Thulani Silinda <silindatn@gmail.com>
2018-05-28 14:52:27 +0200

    style

--- App.js
+++ App.js
@@ -61,10 +61,3 @@ export default class App extends Component {
     );
   }
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    marginTop: 50
-  }
-});
